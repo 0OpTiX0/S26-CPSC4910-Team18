1124be439eacf8272689e0cf7e4fc2d58ad58d07
Joseph Laudati <laudatijoseph@gmail.com>
2026-02-05 18:14:57 -0500

    Switching to Elastic beanstalk

--- app/main.py
+++ app/main.py
@@ -1,33 +1,7 @@
-from typing import Annotated
-from fastapi import FastAPI, Depends
-from sqlalchemy import text
-from sqlmodel import Session, select
-from db import getSession
-from models import(
-        Admin,
-        Sponsor,
-        Sponsor_User,
-        Driver
-        )
+from fastapi import FastAPI
 
-app = FastAPI(
-        docs_url = "/api/docs",
-        openapi_url = "/api/openapi.json",
-        redoc_url="/api/redoc",
+app = FastAPI()
 
-       )
-
-
-
-SessionDep = Annotated[Session, Depends(getSession)]
-
-
-@app.get("/api/admins")
-def getAdmins(*, session: Session = Depends(getSession)):
-       admins = session.exec(select(Admin)).all()
-       return(admins)
-
-@app.post("/api/admins")
-def createAdmin(id : int, name : str, email : str, phone_num : str, hash_key : str, session : Session = Depends(getSession)):
-	return True
-
+@app.get("/health")
+def health():
+    return {"ok": True}
