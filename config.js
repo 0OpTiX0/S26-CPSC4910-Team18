26bb09c8a51b474455856c02fe7a409724f81531
Justin1065 <jyk@clemson.edu>
2026-02-20 16:02:50 -0500

    Updated config file to work both locally and on EB

--- config.js
+++ config.js
@@ -1,2 +1,8 @@
-// Local development ONLY
-window.__API_BASE__ = "http://127.0.0.1:8000";
+// frontend/config.js
+const CONFIG = {
+    API_BASE_URL: window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1"
+        ? "http://127.0.0.1:8000" // Your local FastAPI
+        : "http://team18-env.eba-wdekfmiy.us-east-1.elasticbeanstalk.com" // Your EB Backend
+};
+
+console.log("Running with API URL:", CONFIG.API_BASE_URL);
--- frontend/js/config.js
+++
@@ -1,3 +0,0 @@
-const CONFIG = {
-    API_BASE_URL: "http://team18-env.eba-wdekfmiy.us-east-1.elasticbeanstalk.com"
-};
